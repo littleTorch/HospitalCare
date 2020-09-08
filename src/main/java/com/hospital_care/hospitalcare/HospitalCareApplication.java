4bc83d96f8a5ec57548811a897e56a1c9f48d421
ChenJingZhuo <3279838729@qq.com>
2020-09-08 15:49:10 +0800

    0.0.1

---
+++ src/main/java/com/hospital_care/hospitalcare/HospitalCareApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_care.hospitalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital_care/hospitalcare/HospitalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_care.hospitalcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
